c7c210020a4797eb2bad28e3a49f3351e7120eb1
D-C-C-DT <duotchicuong@gmail.com>
2024-04-04 14:21:56 +0700

    up file

---
+++ WebsiteBanTraSua/WebsiteBanTraSua/Repository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebsiteBanTraSua.Models;
+using WebsiteBanTraSua.Models;
+
+namespace WebsiteBanTraSua.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+
+}
---
+++ WebsiteBanTraSua/WebsiteBanTraSua/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebsiteBanTraSua.Models;
+using WebsiteBanTraSua.Models;
+
+namespace WebsiteBanTraSua.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+
+}
